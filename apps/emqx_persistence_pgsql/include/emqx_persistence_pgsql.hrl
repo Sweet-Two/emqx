d7175ac4afc144cd2e1ad52b3711b68ceafe5e85
Sweet-Two <157085973+Sweet-Two@users.noreply.github.com>
2024-09-20 14:43:37 +0800

    解决执行sql语句提示的语法错误

--- apps/emqx_persistence_pgsql/include/emqx_persistence_pgsql.hrl
+++ apps/emqx_persistence_pgsql/include/emqx_persistence_pgsql.hrl
@@ -2,31 +2,26 @@
 -define(ECPOOL_WORKER, emqx_persistence_pgsql_cli).
 
 -define(INSERT_CONNECT_SQL, <<
-    "INSERT INTO `ota_emqx`.`on_client_connected`(`action`,`node`,`client_id`,`username`,`ip`,`connected_at`)
+    "INSERT INTO on_client_connected(action,node,client_id,username,ip,connected_at)
     VALUES (?,?,?,?,?,?);"
 >>).
 
 -define(INSERT_DISCONNECT_SQL, <<
-    "INSERT INTO `ota_emqx`.`on_client_disconnected`(`action`,`node`,`client_id`,`username`,`reason`,`disconnected_at`)
+    "INSERT INTO on_client_disconnected(action,node,client_id,username,reason,disconnected_at)
     VALUES (?,?,?,?,?,?);"
 >>).
 
--define(INSERT_PUBLISH_SQL, <<
-    "INSERT INTO `ota_emqx`.`on_client_publish`(`action`,`node`,`client_id`,`username`,`topic`,`msg_id`,`payload`,`ts`)
-    VALUES (?,?,?,?,?,?,?,?);"
->>).
-
 -define(INSERT_PUBLISH_MSG_SQL, <<
-    "INSERT INTO `ota_emqx`.`t_publish_msg`(`msg_id`,`sender`,`topic`,`payload`,`create_time`,`sender_ip`)
+    "INSERT INTO t_publish_msg(msg_id,sender,topic,payload,create_time,sender_ip)
     VALUES (?,?,?,?,?,?);"
 >>).
 
 -define(INSERT_CONSUME_MSG_SQL, <<
-    "INSERT INTO `ota_emqx`.`t_consume_msg`(`msg_id`,`client_id`,`create_time`)
+    "INSERT INTO t_consume_msg(msg_id,client_id,create_time)
     VALUES (?,?,?);"
 >>).
 
 -define(INSERT_OFFLINE_MSG_SQL, <<
-    "INSERT INTO `ota_emqx`.`mqtt_msg`(`msg_id`,`sender`,`topic`,`payload`,`arrived`)
+    "INSERT INTO mqtt_msg(msgid,sender,topic,payload,arrived)
     VALUES (?,?,?,?,?);"
 >>).
