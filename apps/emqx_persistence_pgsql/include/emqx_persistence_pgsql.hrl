2b2a83fea10fe0142e2d8e029751c67d0b840f38
Sweet-Two <157085973+Sweet-Two@users.noreply.github.com>
2024-09-25 16:26:57 +0800

    尝试解决insert到pgsql的报错

--- apps/emqx_persistence_pgsql/include/emqx_persistence_pgsql.hrl
+++ apps/emqx_persistence_pgsql/include/emqx_persistence_pgsql.hrl
@@ -2,26 +2,26 @@
 -define(ECPOOL_WORKER, emqx_persistence_pgsql_cli).
 
 -define(INSERT_CONNECT_SQL, <<
-    "INSERT INTO on_client_connected(id,action,node,client_id,username,ip,connected_at)
-    VALUES (DEFAULT,$1,$2,$3,$4,$5,$6);"
+    "INSERT INTO on_client_connected(action,node,client_id,username,ip,connected_at)
+    VALUES ($1,$2,$3,$4,$5,$6);"
 >>).
 
 -define(INSERT_DISCONNECT_SQL, <<
-    "INSERT INTO on_client_disconnected(id,action,node,client_id,username,reason,disconnected_at)
-    VALUES (DEFAULT,$1,$2,$3,$4,$5,$6);"
+    "INSERT INTO on_client_disconnected(action,node,client_id,username,reason,disconnected_at)
+    VALUES ($1,$2,$3,$4,$5,$6);"
 >>).
 
 -define(INSERT_PUBLISH_MSG_SQL, <<
-    "INSERT INTO t_publish_msg(id,msg_id,sender,topic,payload,create_time,sender_ip)
-    VALUES (DEFAULT,$1,$2,$3,$4,$5,$6);"
+    "INSERT INTO t_publish_msg(msg_id,sender,topic,payload,create_time,sender_ip)
+    VALUES ($1,$2,$3,$4,$5,$6);"
 >>).
 
 -define(INSERT_CONSUME_MSG_SQL, <<
-    "INSERT INTO t_consume_msg(id,msg_id,client_id,create_time)
-    VALUES (DEFAULT,$1,$2,$3);"
+    "INSERT INTO t_consume_msg(msg_id,client_id,create_time)
+    VALUES ($1,$2,$3);"
 >>).
 
 -define(INSERT_OFFLINE_MSG_SQL, <<
-    "INSERT INTO mqtt_msg(id,msgid,sender,topic,payload,arrived)
-    VALUES (DEFAULT,$1,$2,$3,$4,$5);"
+    "INSERT INTO mqtt_msg(msgid,sender,topic,payload,arrived)
+    VALUES ($1,$2,$3,$4,$5);"
 >>).
